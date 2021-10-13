f259aee7fbd2909d21a41a223e7a1f330895d54f
Shubh Shukla <62519938+shuklashubh10@users.noreply.github.com>
2021-10-14 01:47:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,56 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="howItContainer"> 
+    <div className="containerkaconatiner">
+      <h1 className="HowitworksHeading">HOW IT WORKS</h1>
+        <div className="bulletpoints">
+          <ul>
+            <li>lorem ipsum is here is there any alternative </li>
+            <li>lorem ipsum is here is there any alternative </li>
+            <li>lorem ipsum is here is there any alternative </li>
+            </ul>
+        </div>
+    </div>
+        
+        <div class="flex-container">
+
+<div class="flex-child magenta">
+<h1 className="Flex-child-heading" > FACTORING/SALE BILL DISCOUNTING</h1>
+
+<p>lorem ipsum is here is there any alternativelorem ipsum is here is there any alternativelorem ipsum is here is there any alternative</p>
+<p>Process</p>
+<ul>
+
+  <li> lorem ipsum is here is there any alternative</li>
+  <li> lorem ipsum is here is there any alternative</li>
+  <li> lorem ipsum is here is there any alternative</li>
+  <li> lorem ipsum is here is there any alternative</li>
+
+  </ul>
+</div>
+
+<div class="flex-child green">
+<h1 className="Flex-child-heading">REVERSEFACTORING/PURCHASE BILL DISCOUNTING</h1>
+<p>lorem ipsum is here is there any alternativelorem ipsum is here is there any alternativelorem ipsum is here is there any alternative</p>
+<p>Process</p>
+<ul>
+
+  <li> lorem ipsum is here is there any alternative</li>
+  <li> lorem ipsum is here is there any alternative</li>
+  <li> lorem ipsum is here is there any alternative</li>
+  <li> lorem ipsum is here is there any alternative</li>
+
+  </ul>
+</div>
+
+</div>
+          </div>
+
+      
+  );
+}
+
+export default App;
